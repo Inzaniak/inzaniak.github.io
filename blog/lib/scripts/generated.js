3e2b4b284bcb0de649e0178eb24e0fc2dfec0d9b
Inzaniak <inzaniak@gmail.com>
2024-01-13 14:45:43 +0100

    added article and updated loras

--- blog/lib/scripts/generated.js
+++ blog/lib/scripts/generated.js
@@ -1,1 +1,1 @@
-let nodes={paths:["articles/the-1shot-lora-guide.html"],nodeCount:1,linkSources:[],linkTargets:[],labels:["The 1shot LoRA Guide"],radii:[null],linkCount:0},attractionForce=1,linkLength=10,repulsionForce=150,centralForce=3,edgePruning=100
+let nodes={paths:["articles/the-workflow-script-comprehensive-guide.html"],nodeCount:1,linkSources:[],linkTargets:[],labels:["The Workflow Script Comprehensive Guide"],radii:[null],linkCount:0},attractionForce=1,linkLength=10,repulsionForce=150,centralForce=3,edgePruning=100
