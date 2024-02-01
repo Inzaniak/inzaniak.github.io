2eb06eae66b1e508daa782a9c621ca65be94992c
Inzaniak <inzaniak@gmail.com>
2024-02-01 15:33:54 +0100

    added pixelart guide and models

--- blog/lib/scripts/generated.js
+++ blog/lib/scripts/generated.js
@@ -1,1 +1,1 @@
-let nodes={paths:["articles/the-workflow-script-comprehensive-guide.html"],nodeCount:1,linkSources:[],linkTargets:[],labels:["The Workflow Script Comprehensive Guide"],radii:[null],linkCount:0},attractionForce=1,linkLength=10,repulsionForce=150,centralForce=3,edgePruning=100
+let nodes={paths:["articles/the-pixel-art-comfyui-workflow-guide.html"],nodeCount:1,linkSources:[],linkTargets:[],labels:["The Pixel Art ComfyUI Workflow Guide"],radii:[null],linkCount:0},attractionForce=1,linkLength=10,repulsionForce=150,centralForce=3,edgePruning=100
